36e95eafdcb490912ba9b983d66aff006d1d8376
vcaruncho <cholinyo@gmail.com>
2019-04-30 23:11:10 +0200

    creation routes and animations with animated

---
+++ 02-spa/src/app/app.routes.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+
+
+
+
+const routes: Routes = [
+ // { path: '', component: Component },
+  { path: 'home', component: HomeComponent },
+  { path: 'heroes', component: HeroesComponent },
+  {path: 'about', component: AboutComponent}
+ // { path: '**', component: PageNotFoundComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
